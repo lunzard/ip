d2b9269ffdc80aa61288e880981f88794beee006
lunzard <60387314+lunzard@users.noreply.github.com>
2020-08-17 12:04:07 +0800

    Update Level-0
    Implement an initial skeletal version of the Duke that simply greets the user and exits.
    I add them as seperate methods for easier modification in the future

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,25 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
+        // the horizontal line is together with 1 line of spacing
+        String horizontal_line = "____________________________________________________________";
+
         System.out.println("Hello from\n" + logo);
+        System.out.println(horizontal_line);
+        greet();
+        System.out.println(horizontal_line);
+        bye();
+        System.out.println(horizontal_line);
+    }
+
+    public static void greet(){
+        String greeting = "Hello! I'm Duke\n"
+                        + "What can I do for you?";
+        System.out.println(greeting);
+    }
+    public static void bye(){
+        String bye_word = "Bye. Hope to see you again soon!";
+        System.out.println(bye_word);
     }
 }
