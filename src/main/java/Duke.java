d5a9d62dca855ccf532743a284e4ee9439ce4db0
lunzard <60387314+lunzard@users.noreply.github.com>
2020-08-23 14:47:20 +0800

    complete Level-1: Greet.Echo.Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,29 +1,45 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+
+        String horizontal_line = "____________________________________________________________";
+        Scanner in = new Scanner(System.in);
+        String userInputLine;
+
+        String exitCommandText = "bye";
+
+        showLogo(horizontal_line);
+        greet(horizontal_line);
+
+        userInputLine = in.nextLine();
+        while(!userInputLine.equalsIgnoreCase(exitCommandText)){
+            echoCommand(userInputLine,horizontal_line);
+            userInputLine = in.nextLine();
+        }
+        bye(horizontal_line);
+    }
+
+    public static void showLogo(String horizontalLine){
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-
-        // the horizontal line is together with 1 line of spacing
-        String horizontal_line = "____________________________________________________________";
-
-        System.out.println("Hello from\n" + logo);
-        System.out.println(horizontal_line);
-        greet();
-        System.out.println(horizontal_line);
-        bye();
-        System.out.println(horizontal_line);
+        System.out.println(horizontalLine+ "\n" + logo);
     }
 
-    public static void greet(){
+    public static void greet(String horizontalLine){
         String greeting = "Hello! I'm Duke\n"
-                        + "What can I do for you?";
-        System.out.println(greeting);
+                        + "What can I do for you?\n";
+        System.out.println(horizontalLine + "\n" + greeting + horizontalLine);
     }
-    public static void bye(){
-        String bye_word = "Bye. Hope to see you again soon!";
-        System.out.println(bye_word);
+    public static void bye(String horizontalLine){
+        String bye_word = "Bye. Hope to see you again soon!\n";
+        System.out.println(horizontalLine + "\n" + bye_word + "\n" + horizontalLine);
+    }
+
+    public static void echoCommand(String userInput, String horizontalLine){
+        System.out.println(horizontalLine + "\n" + userInput + "\n" + horizontalLine);
     }
 }
